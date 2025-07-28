38e902007c74d9b99b2bebdebd8776042589e5ca
prashant2469 <89878340+prashant2469@users.noreply.github.com>
2025-07-28 12:50:35 -0700

    Updated Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -225,7 +225,7 @@ export function Dashboard() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': Bearer ${supabase.supabaseKey}
+          'Authorization': `Bearer ${session.access_token}`
         },
         body: JSON.stringify(base64Data),
       });
